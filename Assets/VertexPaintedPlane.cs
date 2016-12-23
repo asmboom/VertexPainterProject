740e289e2ddc86eb471cffdd61fc35decfcdfc84
jh931223 <jh931223@gmail.com>
2016-12-23 17:24:57 +0900

    first commit
    버텍스컬러!!!

---
+++ Assets/VertexPaintedPlane.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// VertexColor Change
+public class VertexPaintedPlane : MonoBehaviour {
+    
+    public MeshFilter meshFilter;
+
+	void Start () {
+        meshFilter = GetComponent<MeshFilter>();
+        Init();
+	}
+
+    void Init()
+    {
+        Mesh mesh = meshFilter.mesh;
+        Color[] colors = new Color[mesh.vertices.Length];
+        for (int i = 0; i < colors.Length; i++)
+            colors[i] = new Color(1.0f, 1.0f, 1.0f);
+        mesh.colors = colors;
+    }
+	/*void Update () {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, Mathf.Infinity))
+            {
+                int triangle = hitInfo.triangleIndex;
+                print(triangle);
+            }
+        }
+	}*/
+}
---
+++ Assets/VertexPainter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexPainter : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    void SetVertexColor(List<int> triangles,Mesh _targetMesh)
+    {
+        if (triangles == null || triangles.Count <= 0)
+            return;
+        Mesh mesh = _targetMesh;
+        Color[] colors = mesh.colors;
+        foreach (int triangle in triangles.ToArray())
+        {
+            colors[mesh.triangles[triangle * 3]] = new Color(0.0f, 0.0f, 0.0f);
+            colors[mesh.triangles[triangle * 3 + 1]] = new Color(0.0f, 0.0f, 0.0f);
+            colors[mesh.triangles[triangle * 3 + 2]] = new Color(0.0f, 0.0f, 0.0f);
+        }
+        mesh.colors = colors;
+    }
+    void OnCollisionStay(Collision col)
+    {
+        List<int> triangles = new List<int>();
+
+        if (col.gameObject.layer == LayerMask.NameToLayer("ground"))
+        {
+            foreach (ContactPoint P in col.contacts)
+            {
+                RaycastHit hit;
+                Ray ray = new Ray(P.point + P.normal * 0.05f, -P.normal);
+                if (Physics.Raycast(ray, out hit, 0.1f, LayerMask.GetMask("ground")))
+                {
+                    Debug.Log(hit.triangleIndex);
+                    triangles.Add(hit.triangleIndex);
+                }
+            }
+            if (triangles.Count > 0)
+                SetVertexColor(triangles, col.gameObject.GetComponent<MeshFilter>().mesh);
+        }
+    }
+}
